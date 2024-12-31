10f8bbbdca36923fedc0e6b819c1608abe80b90d
Julian <124217830+JulyAn1234@users.noreply.github.com>
2024-12-31 00:40:16 -0800

    Testing pipeline with button color change

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -89,7 +89,7 @@ const LoginPage: React.FC = () => {
             type="submit"
             fullWidth
             variant="contained"
-            color="primary"
+            color="#ffffff"
             sx={{ mt: 3, mb: 2 }}
             disabled={loading}
           >
